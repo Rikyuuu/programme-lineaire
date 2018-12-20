37fc14dd966a305be19e360367bae5eb43fb7dca
Rikyuu <julien.delissenne@gmail.com>
2018-12-20 02:03:29 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContraintesComponent } from './contraintes/contraintes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ContraintesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import { ContraintesComponent } from './contraintes/contraintes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContraintesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
